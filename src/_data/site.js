608f5003d73ba0cfef50ba4672bb45f3ba4d3abe
Paul Libbrecht <paul@hoplahup.net>
2024-01-18 07:48:45 +0100

    Current state

---
+++ src/_data/site.js
@@ -0,0 +1,11 @@
+module.exports = {
+	"title": "Formeleingabe.iu",
+	"description": "Attempting to explore formula input for the IU students and examns.",
+	"url": "https://formeleingabe.iu.org/",
+	"author": "Paul Libbrecht",
+	"meta_data": {
+		"twitter": "@polx",
+		"opengraph_default": "/static/opengraph-default.jpg"
+	},
+	"env": process.env.ELEVENTY_ENV === 'production'
+}
